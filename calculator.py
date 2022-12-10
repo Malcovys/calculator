801bf7b2f1f0f8c4ddb0a13ddacfe29a71fdd845
Malcovys <malcovys22.aps@gmail.com>
2022-12-11 00:43:18 +0300

    add fonctionalities

--- calculator.py
+++ calculator.py
@@ -7,15 +7,31 @@ class MainScreen(BoxLayout):
     pass
 
 class CalculatorApp(App):
+    array = []
     operation = StringProperty("")
     resultat = StringProperty("")
     
-    def on_click_button(self, *args):
-        pass
+    def on_click_buttonon_click_numeric_button(self,id,*args):
+        self.array.append(id.text)
+        self.operation = ''.join(map(str,self.array))# ==> 'salut'
     
-    def calculate(self, *args):
-        pass
+    def delete(self,*args):
+        if self.array:
+            self.resultat = ""
+            self.array.pop()
+            self.operation = ''.join(map(str,self.array))
+    
+    def delete_all(self,*args):
+        self.array.clear()
+        self.operation = ""
+        self.resultat = ""
         
+    def calculate(self,*args):
+        try:
+            self.resultat = str(eval(self.operation))
+        except:
+            self.resultat = "Syntax error"
+                
     def build(self):
         return MainScreen()
 
