0d4c30d4bfa5bd0f98fb043445330a2cf008351e
Malcovys <malcovys22.aps@gmail.com>
2022-12-10 22:51:59 +0300

    add on_press option

--- calculator.py
+++ calculator.py
@@ -1,13 +1,23 @@
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
+from kivy.properties import StringProperty
     
 
-class Touch(BoxLayout):
+class MainScreen(BoxLayout):
     pass
 
 class CalculatorApp(App):
+    operation = StringProperty("")
+    resultat = StringProperty("")
+    
+    def on_click_button(self, *args):
+        pass
+    
+    def calculate(self, *args):
+        pass
+        
     def build(self):
-        return Touch()
+        return MainScreen()
 
 if __name__ == '__main__':
     CalculatorApp().run()
